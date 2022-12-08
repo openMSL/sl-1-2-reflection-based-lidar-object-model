8a6e39f673e0c923f8b74845d2476c5bce08db37
ClemensLinnhoff <clemens.linnhoff@persival.de>
2022-12-08 14:18:29 +0100

    Removed EUPL and replaced all copyright/license headers in files
    Signed-off-by: ClemensLinnhoff <clemens.linnhoff@persival.de>

--- src/osmp/FrameworkPackaging.cpp
+++ src/osmp/FrameworkPackaging.cpp
@@ -1,17 +1,8 @@
-/* FMU Framework for FMI 2.0 Co-Simulation FMUs using FZD strategies
- *
- * (C) 2020 Technical University of Darmstadt
- *
- * based on OSMPDummySensor.cpp from
- *
- * PMSF FMU Framework for FMI 2.0 Co-Simulation FMUs
- *
- * (C) 2016 -- 2018 PMSF IT Consulting Pierre R. Mai
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
+//
+// Copyright 2018 PMSF IT Consulting - Pierre R. Mai
+// Copyright 2022 Technical University of Darmstadt - FZD
+// SPDX-License-Identifier: MPL-2.0
+//
 
 #include "FrameworkPackaging.h"
 
