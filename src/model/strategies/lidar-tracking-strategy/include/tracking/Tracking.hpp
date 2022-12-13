90cb48e7f8fec9fca636c7859f913aa7071f861d
Clemens Linnhoff <clemens.linnhoff@persival.de>
2022-12-13 14:34:47 +0100

    Update licenses (#2)
    * Updated model to newest release on TUDa Gitlab
    
    Signed-off-by: ClemensLinnhoff <clemens.linnhoff@persival.de>
    
    * Removed EUPL and replaced all copyright/license headers in files
    
    Signed-off-by: ClemensLinnhoff <clemens.linnhoff@persival.de>

--- src/model/strategies/lidar-tracking-strategy/include/tracking/Tracking.hpp
+++ src/model/strategies/lidar-tracking-strategy/include/tracking/Tracking.hpp
@@ -1,19 +1,6 @@
 //
-// Copyright Philipp Rosenberger, M. Sc.
-// Institute of Automotive Engineering
-// of Technical University of Darmstadt, 2021.
-// Licensed under the EUPL-1.2-or-later
-//
-// This work covered by the EUPL can be used/merged and distributed
-// in other works covered by GPL-2.0, GPL-3.0, LGPL, AGPL, CeCILL,
-// OSL, EPL, MPL and other licences listed as compatible in the EUPL
-// Appendix. This applies to the other (combined) work, while the
-// original project stays covered by the EUPL without re-licensing.
-//
-// Alternatively, the contents of this file may be used under the
-// terms of the Mozilla Public License, v. 2.0. If a copy of the MPL
-// was not distributed with this file, you can obtain one at
-// http://mozilla.org/MPL/2.0/.
+// Copyright 2022 Technical University of Darmstadt - FZD
+// SPDX-License-Identifier: MPL-2.0
 //
 
 #ifndef TRACKING_STRATEGY_HPP
